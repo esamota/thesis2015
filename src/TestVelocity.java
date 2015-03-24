2fbfb7bc59797faaf4d163d00fc3a8ad72d78061
esamota <esamota@css.edu>
2015-03-24 12:47:45 +0100

    created templates, hashmaps for nodes and edges and two methods: one to use velocity to print BPMN as a string, and another to write it to a file. File prints with correct syntax. Yaoqianq still says it's invalid

---
+++ src/TestVelocity.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+
+
+public class TestVelocity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	}
+	
+	
+
+}
