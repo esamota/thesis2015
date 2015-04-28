1b7fbf0525433df95875745bab82ecabddab1ae8
esamota <esamota@css.edu>
2015-04-28 12:01:27 +0200

    xslt class added

---
+++ src/XSLT.java
@@ -0,0 +1,24 @@
+import javax.xml.transform.*;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import java.io.*;
+
+public class XSLT {
+	
+	static Source xmlInput = new StreamSource(new File("C:\\Users\\Elena\\Desktop\\xLMexamples\\q13.xml"));
+	static Source xsl = new StreamSource(new File("C:\\Users\\Elena\\Desktop\\test.xsl"));
+	static Result bpmnOutput = new StreamResult(new File("C:\\Users\\Elena\\Desktop\\test.bpmn"));
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			Transformer transformer = TransformerFactory.newInstance().newTransformer(xsl);
+		    transformer.transform(xmlInput, bpmnOutput);
+		    }
+		  catch (Exception e) {
+		    e.printStackTrace( );
+		    }	
+	}
+
+}
